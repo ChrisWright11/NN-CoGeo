aa77fc932de8e433f40a8fb4ac8ed869109a3061
songford <songchuning@hotmail.com>
2014-08-04 17:10:38 +1000

    2014/08/04
    Google Map and Autocomplete implemented, but can't get namebar working.
    Name field not posted.

--- CoGeo/dynamodbUploader.php
+++ CoGeo/dynamodbUploader.php
@@ -1,53 +1,188 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Tidus
- * Date: 14-7-28
- * Time: 9:20 p.m.
- */
+<!DOCTYPE html>
 
-require "vendor/autoload.php";
+<html>
+<head>
 
 
-use Aws\DynamoDb\DynamoDbClient;
-use Aws\Common\Enum\Region;
-use Aws\DynamoDb\Enum\Type;
-use Aws\DynamoDb\Enum\AttributeAction;
-use Aws\DynamoDb\Enum\ReturnValue;
+    <title>Submitted!</title>
 
-$client = DynamoDbClient::factory(array(
-    'profile' => 'default',
-    'region' => Region::AP_SOUTHEAST_2
-));
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
-$tableName = "CoGeo_Place_Database";
-$placeName = $_POST["name1"];
-$feeling1 = $_POST["feeling1"];
-$feeling2 = $_POST["feeling2"];
-$feeling3 = $_POST["feeling3"];
-$feeling4 = $_POST["feeling4"];
-$feeling5 = $_POST["feeling5"];
-$feeling6 = $_POST["feeling6"];
-$feeling7 = $_POST["feeling7"];
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <!-- jquery -->
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <!-- jquery UI-->
+    <script src="//code.jquery.com/ui/1.11.0/jqueryui/1.11.0/jquery-ui.js"></script>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
 
-################################################################
-//Adding data to CoGeo Primary Database
+    <style>
 
-echo PHP_EOL;
 
-$response = $client->putItem(array(
-    "TableName" => $tableName,
-    "Item" => $client->formatAttributes(array(
-            "Name" => $placeName,
-            "Feeling1" => $feeling1,
-            "Feeling2" => $feeling2,
-            "Feeling3" => $feeling3,
-            "Feeling4" => $feeling4,
-            "Feeling5" => $feeling5,
-            "Feeling6" => $feeling6,
-            "Feeling7" => $feeling7,
+        body{
+            text-align: center;
+            background: url("http://wallpaperscraft.com/image/mountain_peak_stars_sky_night_light_snow_46057_1920x1200.jpg?orig=1");
+            background-size: cover;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-repeat: no-repeat;
+            background-position: position;
 
-        )),
-    "ReturnConsumedCapacity" => "TOTAL"
-));
+        }
+
+        h1{
+            text-align: center;
+            color: white;
+        }
+
+        p{
+            text-align: center;
+            color: white;
+        }
+
+        h3{
+            text-align: center;
+            color: white;
+        }
+
+    </style>
+
+
+</head>
+
+<body>
+<header class = "navbar">
+    <div class = "container">
+
+
+        <nav class="navbar navbar-inverse" role="navigation">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">CoGeo</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li class="active"><a href="submit.php">Add Spot</a></li>
+                        <li><a href="places.php">See All</a></li>
+
+                    </ul>
+
+
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+    </div>
+</header>
+<div class="container">
+    <h1> Thankyou for your submission!</h1>
+
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: Tidus
+     * Date: 14-7-28
+     * Time: 9:20 p.m.
+     */
+
+    require "vendor/autoload.php";
+
+
+    use Aws\DynamoDb\DynamoDbClient;
+    use Aws\Common\Enum\Region;
+    use Aws\DynamoDb\Enum\Type;
+    use Aws\DynamoDb\Enum\AttributeAction;
+    use Aws\DynamoDb\Enum\ReturnValue;
+
+    $client = DynamoDbClient::factory(array(
+        'key' => 'AKIAIIPESLZS2YXBZNOA',
+        'secret' => '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',
+        'region' => Region::AP_SOUTHEAST_2
+    ));
+
+    $tableName = "CoGeo_Place_Database";
+    $placeName = $_POST["name1"];
+    $feeling1 = $_POST["feeling1"];
+    $feeling2 = $_POST["feeling2"];
+    $feeling3 = $_POST["feeling3"];
+    $feeling4 = $_POST["feeling4"];
+    $feeling5 = $_POST["feeling5"];
+    $feeling6 = $_POST["feeling6"];
+    $feeling7 = $_POST["feeling7"];
+
+
+    ################################################################
+    //Adding data to CoGeo Primary Database
+
+
+    $response = $client->putItem(array(
+        "TableName" => $tableName,
+        "Item" => $client->formatAttributes(array(
+                    "Name" => $placeName,
+                    "Chatty" => $feeling1,
+                    "Buzz" => $feeling2,
+                    "Pump" => $feeling3,
+                    "Adventure" => $feeling4,
+                    "Bustle" => $feeling5,
+                    "LoveyDovey" => $feeling6,
+                    "Trackies" => $feeling7,
+                )
+            ),
+        "ReturnConsumedCapacity" => 'TOTAL',
+        "Expected" => array(
+            "Name" => array(
+                "ComparisonOperator" => "NULL"
+            ))
+    ));
+
+
+    echo "<br />";
+
+
+    ?>
+    <h3></h3>
+    </br>
+    </br>
+
+    <div class="home-btn">
+        </br>
+        <!-- Indicates a successful or positive action -->
+        <a href="index.php"><button type="button" class="btn btn-success btn-lg" onclick="uploadSpot()">Home</button></a>
+        </br>
+    </div>
+
+    </br>
+    </br>
+
+
+</div>
+
+</br>
+</br>
+</br>
+</br>
+
+
+
+</body>
+
+</html>
+
+
+
+
+
--- CoGeo/submit.php
+++ CoGeo/submit.php
@@ -1,204 +1,349 @@
 <!DOCTYPE html>
 
 <html>
-	<head>
-		
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<head>
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-		<!-- jquery -->
-		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-		<!-- jquery UI-->
-		<script src="//code.jquery.com/ui/1.11.0/jqueryui/1.11.0/jquery-ui.js"></script>
+    <title>Submit</title>
 
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	
-		<!-- background line: url("http://wallpaperscraft.com/image/mountain_peak_stars_sky_night_light_snow_46057_1920x1200.jpg?orig=1"); -->
-		<style>
-		
-		
-			body{
-				text-align: center;
-				background: #666;
-				background-size: cover;
-				-webkit-background-size: cover;
-				-moz-background-size: cover;
-				-o-background-size: cover;
-				background-repeat: no-repeat;
-				background-position: position;
-				
-			}
-			
-			h1{
-				text-align: center;
-				color: white;
-			}
-			
-			p{
-				text-align: center;
-				color: white;
-			}
-			
-			h3{
-				text-align: center;
-				color: white;
-			}
-			.input-group{
-				width: 500px;
-				text-align: center;
-			}
-			
-		</style>
-		
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
-	
-	</head>
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <!-- jquery -->
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <!-- jquery UI-->
+    <script src="//code.jquery.com/ui/1.11.0/jqueryui/1.11.0/jquery-ui.js"></script>
 
-	<body>
-		<header class = "navbar">
-			<div class = "container">
-				
-		
-				<nav class="navbar navbar-inverse" role="navigation">
-				  <div class="container-fluid">
-				    <!-- Brand and toggle get grouped for better mobile display -->
-				    <div class="navbar-header">
-				      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				        <span class="sr-only">Toggle navigation</span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				      </button>
-				      <a class="navbar-brand" href="#">CoGeo</a>
-				    </div>
-				
-				    <!-- Collect the nav links, forms, and other content for toggling -->
-				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				      <ul class="nav navbar-nav">
-				        <li><a href="index.php">Home</a></li>
-				        <li class="active"><a href="#">Add Spot</a></li>
-				        <li><a href="places.php">See All</a></li>
-				        
-				      </ul>
-				      
-				      
-				    </div><!-- /.navbar-collapse -->
-				  </div><!-- /.container-fluid -->
-				</nav>
-			</div>
-		</header>
-	<div class="container">
-		<h1> Add your spot!</h1>
-		<p>
-			 Enter the name of your spot below and then rate it on the feelings shown from 0 - 10;
-		</p>
-		<h3></h3>
-	</br>
-	</br>
-	
-	<form action = "dynamodbUploader.php" method = "post" name="name" id="uploadForm">
-
-		<div class="input-group">
-		  <span class="input-group-addon">Place Name</span>
-		  <input id="name1" input type="text" class="form-control" name="name1" placeholder="Enter Here">
-		</div>
-
-	<br/>
-		<h3>Feeling 1</h3>
-		<br/>	
-			<input id="feeling1" input type="range" name = "feeling1" min="0" max="10" value="5" step="1" onchange="showValue(this)"/>
-		<p><span id="range">5</span></p>
-		</br>
-	<script type="text/javascript">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
 
-	function get_nextsibling(n) {
-    	x=n.nextSibling;
- 			while (x.nodeType!=1) {
-    	x=x.nextSibling; }
- 			return x; }
+    <!-- background line: url("http://wallpaperscraft.com/image/mountain_peak_stars_sky_night_light_snow_46057_1920x1200.jpg?orig=1"); -->
+    <style>
 
-	function showValue(self) {
-    	get_nextsibling(self).innerHTML=self.value; }
-	</script>
 
-	
+        body {
+            text-align: center;
+            background: url("http://wallpaperscraft.com/image/mountain_peak_stars_sky_night_light_snow_46057_1920x1200.jpg?orig=1");
+            background-size: cover;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-repeat: no-repeat;
+            background-position: position;
+        }
 
-	<br/>
-		<h3>Feeling 2</h3>
-		<br/>	
-			<input id="feeling2" input type="range" name = "feeling2" min="0" max="10" value="5" step="1" onchange="showValue(this)"/>
-			<p><span id="range">5</span></p>
-		</br>
+        .controls {
+            margin-top: 16px;
+            border: 1px solid transparent;
+            border-radius: 2px 0 0 2px;
+            box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            height: 32px;
+            outline: none;
+            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+        }
 
-		<br/>
-		<h3>Feeling 3</h3>
-			<br/>	
-			<input id="feeling3" input type="range" name = "feeling3" min="0" max="10" value="5" step="1" onchange="showValue(this)"/>
-			<p><span id="range">5</span></p>
-			</br>
+        #map-canvas {
+            height: 300px;
+            margin: 0px;
+            padding: 0px;
+        }
 
-		<br/>
-		<h3>Feeling 4</h3>
-			<br/>	
-			<input id="feeling4" input type="range" name = "feeling4" min="0" max="10" value="5" step="1" onchange="showValue(this)"/>
-			<p><span id="range">5</span></p>
-			</br>
+        #place-input {
+            background-color: #fff;
+            padding: 0 11px 0 13px;
+            width: 400px;
+            font-family: Roboto;
+            font-size: 15px;
+            font-weight: 3000;
+            text-overflow: ellipsis;
+        }
 
-		<br/>
-		<h3>Feeling 5</h3>
-			<br/>	
-			<input id="feeling5" input type="range" name = "feeling5" min="0" max="10" value="5" step="1" onchange="showValue(this)"/>
-			<p><span id="range">5</span></p>
-		</br>
-	</form>
+        #place-input:focus {
+            border-color: #4d90fe;
+            margin-left: -1px;
+            padding-left: 14px;
+            width: 401px;
+        }
 
-		<br/>
-		<h3>Feeling 6</h3>
-			<br/>	
-			<input id="feeling6" input type="range" name = "feeling6" min="0" max="10" value="5" step="1" onchange="showValue(this)"/>
-			<p><span id="range">5</span></p>
-			</br>
-	
-	
+        h1 {
+            text-align: center;
+            color: white;
+        }
 
-		<br/>
-		<h3>Feeling 7</h3>
-			<br/>	
-			<input id="feeling7" input type="range" name = "feeling7" min="0" max="10" value="5" step="1" onchange="showValue(this)"/>
-			<p><span id="range">5</span></p>
-			</br>
-	</form>
-	
-	
+        p {
+            text-align: center;
+            color: white;
+        }
 
-	</div>
-	
-	</br>
-	</br>
-	
-   
-	<div class="submit-btn">	
-	</br>
-		<!-- Indicates a successful or positive action -->
-		<a href="#"><button type="button" class="btn btn-success btn-lg" onclick="uploadSpot()">Submit!</button></a>
-	</br>
-	</div>
-	<br/><br/>
-	
-	<script type="text/javascript">
-		
-		function uploadSpot(){
-            document.getElementById("uploadForm").submit();
+        h3 {
+            text-align: center;
+            color: white;
         }
 
 
-		
-	</script>
-	
-	</body>
-    
+
+    </style>
+
+
+</head>
+
+
+<body onload="initialize()">
+<header class="navbar">
+    <div class="container">
+
+
+        <nav class="navbar navbar-inverse" role="navigation">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">CoGeo</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li class="active"><a href="#">Add Spot</a></li>
+                        <li><a href="places.php">See All</a></li>
+
+                    </ul>
+
+
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+    </div>
+</header>
+
+<div class="container">
+
+    <h1> Add your spot!</h1>
+
+    <p>
+        Enter the name of your spot below and then rate it on the feelings shown from 0 - 10;
+    </p>
+
+    <div id="map-canvas"></div>
+    <h3></h3>
+    </br>
+    </br>
+
+    <form action="dynamodbUploader.php" method="post" name="name" id="uploadForm">
+
+
+        <input id="place-input" type="text" class="controls" name="name1" placeholder="Enter Here">
+
+
+        <br/>
+
+        <h3>Chatty</h3>
+        <br/>
+
+        <p>Quiet Time -----> Conversational -----> Outgoing</p>
+        <input id="feeling1" input type="range" name="feeling1" min="0" max="10" value="5" step="1"
+               onchange="showValue(this)"/>
+
+        <p><span id="range">5</span></p>
+        </br>
+        <script type="text/javascript">
+
+
+            function get_nextsibling(n) {
+                x = n.nextSibling;
+                while (x.nodeType != 1) {
+                    x = x.nextSibling;
+                }
+                return x;
+            }
+
+            function showValue(self) {
+                get_nextsibling(self).innerHTML = self.value;
+            }
+        </script>
+
+
+        <br/>
+
+        <h3>Buzz</h3>
+        <br/>
+
+        <p>Relaxed -----> Eager -----> Wild</p>
+        <input id="feeling2" input type="range" name="feeling2" min="0" max="10" value="5" step="1"
+               onchange="showValue(this)"/>
+
+        <p><span id="range">5</span></p>
+        </br>
+
+        <br/>
+
+        <h3>Pump</h3>
+        <br/>
+
+        <p>Rest -----> Sweaty -----> Intense</p>
+        <input id="feeling3" input type="range" name="feeling3" min="0" max="10" value="5" step="1"
+               onchange="showValue(this)"/>
+
+        <p><span id="range">5</span></p>
+        </br>
+
+        <br/>
+
+        <h3>Adventure</h3>
+        <br/>
+
+        <p>Comfortable -----> Explorative -----> Fearless</p>
+        <input id="feeling4" input type="range" name="feeling4" min="0" max="10" value="5" step="1"
+               onchange="showValue(this)"/>
+
+        <p><span id="range">5</span></p>
+        </br>
+
+        <br/>
+
+        <h3>Bustle</h3>
+        <br/>
+
+        <p>Lone Wolf -----> Amongst It -----> Sardine Can</p>
+        <input id="feeling5" input type="range" name="feeling5" min="0" max="10" value="5" step="1"
+               onchange="showValue(this)"/>
+
+        <p><span id="range">5</span></p>
+        </br>
+
+
+        <br/>
+
+        <h3>Lovey Dovey</h3>
+        <br/>
+
+        <p>Platonic -----> Flirty -----> Intimate</p>
+        <input id="feeling6" input type="range" name="feeling6" min="0" max="10" value="5" step="1"
+               onchange="showValue(this)"/>
+
+        <p><span id="range">5</span></p>
+        </br>
+
+
+        <br/>
+
+        <h3>Trackies</h3>
+        <br/>
+
+        <p>Casual -----> Smart -----> Formal</p>
+        <input id="feeling7" input type="range" name="feeling7" min="0" max="10" value="5" step="1"
+               onchange="showValue(this)"/>
+
+        <p><span id="range">5</span></p>
+        </br>
+    </form>
+
+
+</div>
+
+</br>
+</br>
+
+
+<div class="submit-btn">
+    </br>
+    <!-- Indicates a successful or positive action -->
+    <a href="#">
+        <button type="button" class="btn btn-success btn-lg" onclick="uploadSpot()">Submit!</button>
+    </a>
+    </br>
+</div>
+<br/><br/>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+<script type="text/javascript">
+
+    function uploadSpot() {
+        document.getElementById("uploadForm").submit();
+    }
+
+    function initialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(-37.8688, 144.2195),
+            zoom: 13
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+
+        var input = document.getElementById('place-input');
+
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.setTypes(['establishment']);
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+            map: map,
+            anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            infowindow.close();
+            marker.setVisible(false);
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                return;
+            }
+
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);  // Why 17? Because it looks good.
+            }
+            marker.setIcon(/** @type {google.maps.Icon} */({
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(35, 35)
+            }));
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+
+            var address = '';
+            if (place.address_components) {
+                address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || '')
+                ].join(' ');
+            }
+
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+            infowindow.open(map, marker);
+        });
+
+
+    }
+
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+</script>
+
+
+</body>
+
 </html>
